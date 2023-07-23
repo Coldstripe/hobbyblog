14859cee6a9e060bbfe57c91892bbc6de8444c9a
Cameron Thacker <thackercameron60@gmail.com>
2023-07-23 02:30:28 -0400

    Added Firebase config

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,15 @@ import ProfilePic from './components/ProfilePic'
 import {initializeApp} from 'firebase/app'
 
 const inter = Inter({ subsets: ['latin'] })
-const firebaseConfig = {}
+const firebaseConfig = {
+  apiKey: "AIzaSyDDyJfDJqDj9neh_is_nu1inOLm7npGmeo",
+  authDomain: "coldstripe-d86c6.firebaseapp.com",
+  projectId: "coldstripe-d86c6",
+  storageBucket: "coldstripe-d86c6.appspot.com",
+  messagingSenderId: "232109262081",
+  appId: "1:232109262081:web:627735f3463a49f4d84700",
+  measurementId: "G-R0H7Y08SE9"
+};
 const app = initializeApp(firebaseConfig)
 
 export const metadata: Metadata = {
