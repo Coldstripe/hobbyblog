a1f74c768b912a9da1c7eb2d7ffd59e9c6cfd0d0
Cameron Thacker <thackercameron60@gmail.com>
2023-07-25 21:24:25 -0400

    Initialized Vercel Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,29 +1,31 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import Navbar from './components/Navbar'
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import Navbar from "./components/Navbar";
+import { Analytics } from "@vercel/analytics/react";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 //By Cameron Thacker
 export const metadata: Metadata = {
-  title: 'Coldstripe\'s Hobby Blog',
-  description: 'A completely normal hobby blog.',
-}
+  title: "Coldstripe's Hobby Blog",
+  description: "A completely normal hobby blog.",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className= 'grid bg-stone-200 dark:bg-slate-800'>
+    <html lang="en" className="grid bg-stone-200 dark:bg-slate-800">
+      <Analytics />
       <body>
-        <Navbar/>
-        <main className='px-4 md:px-6 prose prose-xl prose-slate dark:prose-invert mx-auto'>
+        <Navbar />
+        <main className="px-4 md:px-6 prose prose-xl prose-slate dark:prose-invert mx-auto">
           {children}
         </main>
       </body>
     </html>
-  )
+  );
 }
