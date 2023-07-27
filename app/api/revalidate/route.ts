aed489df6baed1cf8bd47cd6693cfbb6b9afa522
Cameron Thacker <thackercameron60@gmail.com>
2023-07-26 22:42:07 -0400

    Revalidate as POST

--- app/api/revalidate/route.ts
+++ app/api/revalidate/route.ts
@@ -20,3 +20,23 @@ export async function GET(request: NextRequest) {
 
     return NextResponse.json({ revalidated: true })
 }
+
+export async function POST(request: NextRequest) {
+    const secret = request.nextUrl.searchParams.get('secret')
+
+    if (secret !== process.env.MY_SECRET_TOKEN) {
+        return new NextResponse(JSON.stringify({ message: 'Invalid Token' }), {
+            status: 401,
+            statusText: 'Unauthorized',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+    }
+
+    const path = request.nextUrl.searchParams.get('path') || '/'
+
+    revalidatePath(path)
+
+    return NextResponse.json({ revalidated: true })
+}
