273990b05cb88d2c370d22c253b93a1f2857fffc
Cameron Thacker <thackercameron60@gmail.com>
2023-07-27 00:40:19 -0400

    Updated POST request for webhook

--- app/api/revalidate/route.ts
+++ app/api/revalidate/route.ts
@@ -1,10 +1,21 @@
 import { NextRequest, NextResponse } from 'next/server'
 import { revalidatePath } from 'next/cache'
 
-export async function GET(request: NextRequest) {
-    const secret = request.nextUrl.searchParams.get('secret')
+export async function POST(request: NextRequest) {
+    const secret = request.headers.get('X-Hub-Signature')
+    const crypto = require('crypto');
+    if(!secret){
+        return new NextResponse(JSON.stringify({ message: 'Invalid Token' }), {
+            status: 401,
+            statusText: 'Unauthorized',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+    }
+    const compare = Buffer.byteLength(secret, 'utf-8') === Buffer.byteLength(`sha1=${process.env.GITHUB_SECRET_HASH}`, 'utf-8') && crypto.timingSafeEqual(Buffer.from(secret, 'utf-8'), Buffer.from(`sha1=${process.env.GITHUB_SECRET_HASH}`, 'utf-8'));
 
-    if (secret !== process.env.MY_SECRET_TOKEN) {
+    if (!compare) {
         return new NextResponse(JSON.stringify({ message: 'Invalid Token' }), {
             status: 401,
             statusText: 'Unauthorized',
